e8372def228f75adfe5e61994df397cf68644fea
arjvillan <villanthanam@gmail.com>
2021-05-27 20:43:04 +0200

    did some UserServiceTests

--- src/test/java/ch/uzh/ifi/hase/soprafs21/service/UserServiceTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs21/service/UserServiceTest.java
@@ -12,6 +12,9 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class UserServiceTest {
@@ -82,6 +85,65 @@ public class UserServiceTest {
 
         // then -> attempt to create second user with same user -> check that an error is thrown
         assertThrows(ResponseStatusException.class, () -> userService.createUser(testUser));
+    }
+
+    @Test
+    public void getSortedUserListTest() {
+        // given -> a first user has already been created
+        User testUser2 = new User();
+        testUser2.setId(2L);
+        testUser2.setUsername("testUsername2");
+        testUser2.setToken("2");
+        testUser2.setOnlineStatus(OnlineStatus.ONLINE);
+        testUser2.setPassword("TestPassword2");
+        testUser2.setCreatedOn();
+        testUser2.setGuessedOtherPicturesCorrectly(1);
+        testUser2.setOwnPicturesCorrectlyGuessed(1);
+        testUser2.setPlayerStatus(PlayerStatus.READY);
+        testUser2.setScore(5);
+
+        userRepository.saveAndFlush(testUser);
+        userRepository.saveAndFlush(testUser2);
+
+        List<User> userList = new ArrayList<>();
+        userList.add(testUser2);
+        userList.add(testUser);
+
+
+        // when -> setup additional mocks for UserRepository
+        Mockito.when(userService.getSortedUsers()).thenReturn(userList);
+
+        // then -> attempt to create second user with same user -> check that an error is thrown
+        assert(userList.get(0).getScore() > userList.get(1).getScore());
+    }
+
+    @Test
+    public void getUserListTest() {
+        // given -> a first user has already been created
+        User testUser2 = new User();
+        testUser2.setId(2L);
+        testUser2.setUsername("testUsername2");
+        testUser2.setToken("2");
+        testUser2.setOnlineStatus(OnlineStatus.ONLINE);
+        testUser2.setPassword("TestPassword2");
+        testUser2.setCreatedOn();
+        testUser2.setGuessedOtherPicturesCorrectly(1);
+        testUser2.setOwnPicturesCorrectlyGuessed(1);
+        testUser2.setPlayerStatus(PlayerStatus.READY);
+        testUser2.setScore(1);
+
+        userRepository.saveAndFlush(testUser);
+        userRepository.saveAndFlush(testUser2);
+
+        List<User> userList = new ArrayList<>();
+        userList.add(testUser);
+        userList.add(testUser2);
+
+        // when -> setup additional mocks for UserRepository
+        Mockito.when(userService.getUsers()).thenReturn(userList);
+
+        // then -> attempt to create second user with same user -> check that an error is thrown
+        assert(userList.size()>0);
     }
 
 
