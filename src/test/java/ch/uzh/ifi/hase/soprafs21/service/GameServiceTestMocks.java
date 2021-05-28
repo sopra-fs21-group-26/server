450adc19866f387df0b8f57084f2712dec1c53de
mzehnde <71386295+mzehnde@users.noreply.github.com>
2021-05-28 12:14:27 +0200

    added final gameServiceTests

--- src/test/java/ch/uzh/ifi/hase/soprafs21/service/GameServiceTestMocks.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs21/service/GameServiceTestMocks.java
@@ -3,10 +3,7 @@ package ch.uzh.ifi.hase.soprafs21.service;
 import ch.uzh.ifi.hase.soprafs21.constant.LobbyStatus;
 import ch.uzh.ifi.hase.soprafs21.constant.OnlineStatus;
 import ch.uzh.ifi.hase.soprafs21.constant.PlayerStatus;
-import ch.uzh.ifi.hase.soprafs21.entity.Game;
-import ch.uzh.ifi.hase.soprafs21.entity.Lobby;
-import ch.uzh.ifi.hase.soprafs21.entity.Picture;
-import ch.uzh.ifi.hase.soprafs21.entity.User;
+import ch.uzh.ifi.hase.soprafs21.entity.*;
 import ch.uzh.ifi.hase.soprafs21.repository.GameRepository;
 import ch.uzh.ifi.hase.soprafs21.repository.LobbyRepository;
 import ch.uzh.ifi.hase.soprafs21.repository.PictureRepository;
@@ -52,13 +49,29 @@ public class GameServiceTestMocks {
 
     private Lobby testLobby;
     private User testUser;
+    private User testUser2;
     private Game testGame;
     private List<Picture> picturesOnGrid;
-    private Picture picture;
+    private Picture picture1;
+    private Picture picture2;
 
     @BeforeEach
     public void setup() {
         MockitoAnnotations.openMocks(this);
+
+        picture1 = new Picture();
+        picture1.setId(1L);
+        picture1.setCoordinate("T1");
+        picture1.setUrl("testURL");
+        picturesOnGrid = new ArrayList<>();
+        picturesOnGrid.add(picture1);
+
+        picture2 = new Picture();
+        picture2.setId(1L);
+        picture2.setCoordinate("T1");
+        picture2.setUrl("testURL");
+        picturesOnGrid = new ArrayList<>();
+        picturesOnGrid.add(picture2);
 
         testUser = new User();
         testUser.setId(1L);
@@ -71,9 +84,24 @@ public class GameServiceTestMocks {
         testUser.setOwnPicturesCorrectlyGuessed(1);
         testUser.setPlayerStatus(PlayerStatus.FINISHED);
         testUser.setScore(1);
+        testUser.setRecreatedPicture(picture1);
+
+        testUser2 = new User();
+        testUser2.setId(1L);
+        testUser2.setUsername("testUsername2");
+        testUser2.setToken("2");
+        testUser2.setOnlineStatus(OnlineStatus.ONLINE);
+        testUser2.setPassword("TestPassword2");
+        testUser2.setCreatedOn();
+        testUser2.setGuessedOtherPicturesCorrectly(1);
+        testUser2.setOwnPicturesCorrectlyGuessed(1);
+        testUser2.setPlayerStatus(PlayerStatus.FINISHED);
+        testUser2.setScore(1);
+        testUser2.setRecreatedPicture(picture2);
 
         List<User> testList = new ArrayList<>();
         testList.add(testUser);
+        testList.add(testUser2);
 
         testLobby = new Lobby();
         testLobby.setAdmin(testUser);
@@ -85,16 +113,10 @@ public class GameServiceTestMocks {
         testLobby.setAllAreReadyForNextRound(false);
         testLobby.setIsEndGame(false);
 
-        picture = new Picture();
-        picture.setId(1L);
-        picture.setCoordinate("T1");
-        picture.setUrl("testURL");
-        picturesOnGrid = new ArrayList<>();
-        picturesOnGrid.add(picture);
-
         testGame = new Game(testLobby.getLobbyId(), testList);
         testGame.setGameName("testGame");
         testGame.setPicturesonGrid(picturesOnGrid);
+        testGame.setPlayersInGame(testList);
     }
 
     @Test
@@ -127,6 +149,15 @@ public class GameServiceTestMocks {
         List<Picture> grid = gameService.makeGrid(testLobby.getLobbyId());
 
         assertEquals(16, grid.size());
+    }
+
+    @Test
+    public void getGuessScreen_successful() throws IOException, ParseException {
+        Mockito.when(gameRepository.findByGameId(Mockito.anyLong())).thenReturn(testGame);
+
+        GuessScreen guessScreen = gameService.getGuessScreen(testLobby.getLobbyId(), testUser);
+
+        assertEquals(1, guessScreen.getRecreatedPictures().size());
     }
 
 
